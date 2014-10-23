51cc1612fe65a9804b84c68cee0bc2d25a5b6944
Thomas Cerqueus <thomas.cerqueus@ucd.ie>
2014-10-23 16:13:13 +0200

    Added the files

---
+++ src/com/example/player/Player.java
@@ -0,0 +1,64 @@
+package com.example.player;
+
+import com.example.util.ObjectifyEntity;
+import com.googlecode.objectify.annotation.Entity;
+
+@Entity
+public class Player extends ObjectifyEntity {
+	
+	protected String name;
+	protected Integer rank;
+	protected Float value;
+	protected String type;
+	
+	public Player(String name, Integer rank, Float value, String type) {
+		this.name = name;
+		this.rank = rank;
+		this.value = value;
+		this.type = type;
+	}
+
+	public Player(){
+		name = "";
+		rank = -1;
+		value = new Float(-1);
+		type = "";
+	}
+	
+	public String toString(){
+		return name+", "+rank+", "+value+", "+type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getRank() {
+		return rank;
+	}
+
+	public void setRank(Integer rank) {
+		this.rank = rank;
+	}
+
+	public Float getValue() {
+		return value;
+	}
+
+	public void setValue(Float value) {
+		this.value = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
